c0b9d023960395f0d8b96ccfbc1a4c1f61629fdb
yamllists <118260833+gitworkflows@users.noreply.github.com>
2024-07-15 01:02:07 +0600

    init commit

---
+++ utils/index.ts
@@ -0,0 +1,11 @@
+/** Theme */
+export const STORAGE_THEME_KEY = '__gpt_db_theme_key';
+/** Language */
+export const STORAGE_LANG_KEY = '__gpt_db_lng_key';
+/** Init Message */
+export const STORAGE_INIT_MESSAGE_KET = '__gpt_db_im_key';
+/** Flow nodes */
+export const FLOW_NODES_KEY = '__gpt_db_static_flow_nodes_key';
+
+export * from './storage';
+export * from './constants';
